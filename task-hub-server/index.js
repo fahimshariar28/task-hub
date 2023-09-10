619b6674f5b041ac06b6c832e7a0127f7de8d8e6
Fahim Shariar <fahimshariar28@gmail.com>
2023-09-10 22:13:50 +0600

    Task Hub server setup

---
+++ task-hub-server/index.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const cors = require("cors");
+require("dotenv").config();
+const app = express();
+const port = process.env.PORT || 5000;
+
+// Middleware
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("Task Hub Server");
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on port: ${port}`);
+});
