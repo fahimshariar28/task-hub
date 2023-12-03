72e57136374c779b75db3b07675d53b040a9fb97
Fahim Shariar <fahimshariar28@gmail.com>
2023-12-03 21:59:30 +0600

    Home Page - Task Hub

---
+++ task-hub-client/src/Pages/Home/Home.jsx
@@ -0,0 +1,17 @@
+import About from "../../components/About";
+import Faq from "../../components/FAQ";
+import Trusted from "../../components/Trusted";
+import Hero from "./../../components/Hero";
+
+const Home = () => {
+  return (
+    <div>
+      <Hero />
+      <About />
+      <Trusted />
+      <Faq />
+    </div>
+  );
+};
+
+export default Home;
